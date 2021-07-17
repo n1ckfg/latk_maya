a052f52fad918f89dfb8e10d290e3eb571d76624
Nick Fox-Gieg <nick@fox-gieg.com>
2021-07-16 21:41:39 -0400

    replace submodule

--- build/build.bat
+++ build/build.bat
@@ -1,11 +1,11 @@
 @echo off
 
-set BUILD_TARGET=..\latk_blender.py
+set BUILD_TARGET=..\latk_maya.py
 cd /D %~dp0
 
 del %BUILD_TARGET%
 
-copy /b latk.py\latk.py+latk_main.py+latk_tools.py+latk_rw.py+latk_mtl.py+latk_mesh.py+latk_draw.py+latk_shortcuts.py+latk_ui.py+latk_tilt.py+latk_svg.py %BUILD_TARGET%
+copy /b latkpy\latk.py+latk_main.py+latk_tools.py+latk_rw.py+latk_mtl.py+latk_mesh.py+latk_draw.py+latk_shortcuts.py+latk_ui.py+latk_tilt.py+latk_svg.py %BUILD_TARGET%
 
 rem copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.77\scripts\addons"
 rem copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.78\scripts\addons"
@@ -13,9 +13,9 @@ rem copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2
 rem copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.80\scripts\addons"
 rem copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.81\scripts\addons"
 rem copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.82\scripts\addons"
-copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.83\scripts\addons"
-copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.90\scripts\addons"
-copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.91\scripts\addons"
-copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.92\scripts\addons"
+rem copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.83\scripts\addons"
+rem copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.90\scripts\addons"
+rem copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.91\scripts\addons"
+rem copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.92\scripts\addons"
 
 @pause
